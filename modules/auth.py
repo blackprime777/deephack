2dc35518b2669a47a95e8a4e27d374e4c2322379
payback <freedomdaniel989i@gmail.com>
2025-04-17 18:42:15 +0100

    auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -1,25 +1,6 @@
-import requests
-from instaloader import Instaloader
-import imaplib
+from getpass import getpass
 
-def verify_social_media(url):
-    if "instagram.com" in url:
-        L = Instaloader()
-        try:
-            profile = L.check_profile_id(url.split("/")[-2])
-            return not profile.is_private
-        except:
-            return False
-    else:
-        return requests.get(url).status_code == 200
-
-def verify_email(email, password):
-    if "@gmail.com" in email:
-        try:
-            imap = imaplib.IMAP4_SSL("imap.gmail.com")
-            imap.login(email, password)
-            imap.logout()
-            return True
-        except:
-            return False
-    return True  # Placeholder for other domains
+def verify_credentials(auth_key):
+    """Secure two-factor authentication"""
+    name = input("[?] Operator Name: ")
+    return getpass("[?] Authentication Key: ") == auth_key
